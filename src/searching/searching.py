f46f61dd09ea074062b4495205e32b66a53baa7f
Raymond Thurman <raymondthurman5@gmail.com>
2020-07-08 14:57:21 -0400

    :fire: tests passing for searching file

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,28 @@
 def linear_search(arr, target):
     # Your code here
-
+    for num in arr:
+        if num == target:
+            return arr.index(num)
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        middle = (low + high) // 2
+        guess = arr[middle]
+
+        if guess == target:
+            return arr.index(guess)
+
+        if guess > target:
+            high = middle - 1
+        else:
+            low = middle + 1
 
     return -1  # not found
